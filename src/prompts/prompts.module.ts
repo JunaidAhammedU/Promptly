4f4198fe329f60f783ade3c912a3a446f6159d72
JunaidAhammedU <junaidahammed15@gmail.com>
2025-09-29 13:32:23 +0530

    remove unused spec files and add repository classes for auth and prompts modules

--- src/prompts/prompts.module.ts
+++ src/prompts/prompts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PromptsService } from './prompts.service';
 import { PromptsController } from './prompts.controller';
+import { Repository } from './repository/repository';
 
 @Module({
   controllers: [PromptsController],
-  providers: [PromptsService],
+  providers: [PromptsService, Repository],
 })
 export class PromptsModule {}
