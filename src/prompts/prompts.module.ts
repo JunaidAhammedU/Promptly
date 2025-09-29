9e1226ea58f9372f639e224644f0a42f4df82d11
JunaidAhammedU <junaidahammed15@gmail.com>
2025-09-29 13:47:42 +0530

    update greeting message and improve code formatting

--- src/prompts/prompts.module.ts
+++ src/prompts/prompts.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PromptsService } from './prompts.service';
 import { PromptsController } from './prompts.controller';
-import { Repository } from './repository/repository';
+import { Repository } from './repository';
 
 @Module({
   controllers: [PromptsController],
   providers: [PromptsService, Repository],
 })
-export class PromptsModule {}
+export class PromptsModule { }
