b6d545951e245a924a9d1887d4731a94968c22a4
JunaidAhammedU <junaidahammed15@gmail.com>
2025-10-04 11:03:24 +0530

    Add Prisma module and service for database management and connection handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PromptsModule } from './prompts/prompts.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PromptsModule],
+  imports: [PrismaModule, UsersModule, AuthModule, PromptsModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
-
