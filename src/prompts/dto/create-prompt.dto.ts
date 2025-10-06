cffe5ffb35501607915c2e517e7942f78179268c
JunaidAhammedU <junaidahammed15@gmail.com>
2025-10-06 11:25:48 +0530

    Refactor CreatePromptDto: define properties with validation decorators and remove unused Prompt entity

--- src/prompts/dto/create-prompt.dto.ts
+++ src/prompts/dto/create-prompt.dto.ts
@@ -1,1 +1,39 @@
-export class CreatePromptDto {}
+import { Optional } from "@nestjs/common";
+import { IsArray, IsBoolean, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreatePromptDto {
+
+    @IsString()
+    @MinLength(3)
+    @MaxLength(500)
+    title: string;
+
+    @IsString()
+    @MinLength(10)
+    @MaxLength(2500)
+    content: string;
+
+    @IsString()
+    exampleOutput: string;
+
+    @Optional()
+    @IsString()
+    category: string;
+
+    @IsBoolean()
+    isPublic: boolean;
+
+    @IsString()
+    autherId: string;
+
+    @Optional()
+    @IsString()
+    auther: string;
+
+    @Optional()
+    @IsArray()
+    @IsString({ each: true })
+    @MinLength(1, { each: true })
+    @MaxLength(15, { each: true })
+    tags: string[];
+}
